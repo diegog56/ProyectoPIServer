e9c6a6886d2fa3171e088c106a7c5065962d2e0e
DiegoG56 <gomezefdiego@gmail.com>
2020-03-18 13:31:46 -0600

    Default insertions

---
+++ sql/dbdefaultroles.sql
@@ -0,0 +1,7 @@
+SET @@auto_increment_offset=1;
+SET @@auto_increment_increment=1;
+INSERT INTO rol(nombre) VALUES ('Vendedor');--1
+INSERT INTO rol(nombre) VALUES ('Bodeguero');--2
+INSERT INTO rol(nombre) VALUES ('Repartidor');--3
+INSERT INTO rol(nombre) VALUES ('ESede');--4
+INSERT INTO rol(nombre) VALUES ('EBodega');--5
---
+++ sql/dbdroptables.sql
@@ -0,0 +1,29 @@
+SET foreign_key_checks = 0;
+-- Drop tables
+DROP TABLE bodega;
+
+DROP TABLE categoria;
+
+DROP TABLE cliente;
+
+DROP TABLE departamento;
+
+DROP TABLE detalle_productocategoria;
+
+DROP TABLE detalle_rol;
+
+DROP TABLE detalle_venta;
+
+DROP TABLE municipio;
+
+DROP TABLE producto;
+
+DROP TABLE rol;
+
+DROP TABLE sede;
+
+DROP TABLE usuario;
+
+DROP TABLE venta;
+
+SET foreign_key_checks = 1;
