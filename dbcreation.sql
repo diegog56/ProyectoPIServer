96891729329e6073bfc0094a1b9ed1ce43f23fc6
DiegoG56 <gomezefdiego@gmail.com>
2020-03-18 12:35:57 -0600

    DB Created

---
+++ dbcreation.sql
@@ -0,0 +1,154 @@
+CREATE TABLE rol (
+    id_rol   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre   VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE categoria (
+    id_categoria   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre         VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE departamento (
+    id_departamento   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre            VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE municipio (
+    id_municipio                   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre                         VARCHAR(200) NOT NULL,
+    id_departamento   INTEGER NOT NULL
+);
+
+CREATE TABLE bodega (
+    id_bodega            INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre               VARCHAR(200) NOT NULL,
+    estado               CHAR(1) NOT NULL,
+    id_sede         INTEGER NOT NULL,
+    id_usuario   INTEGER NOT NULL
+);
+
+
+CREATE TABLE cliente (
+    id_cliente     INTEGER NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+    nit            VARCHAR(50) NOT NULL UNIQUE,
+    dpi            INTEGER NOT NULL UNIQUE,
+    direccion      VARCHAR(500) NOT NULL,
+    id_sede   INTEGER NOT NULL
+);
+
+
+CREATE TABLE detalle_productocategoria (
+    id_producto     INTEGER NOT NULL,
+    id_categoria   INTEGER NOT NULL
+);
+
+ALTER TABLE detalle_productocategoria ADD CONSTRAINT detalle_productocategoria_pk PRIMARY KEY ( id_producto,
+                                                                                                id_categoria );
+
+CREATE TABLE detalle_rol (
+    id_usuario   INTEGER NOT NULL,
+    id_rol           INTEGER NOT NULL
+);
+
+ALTER TABLE detalle_rol ADD CONSTRAINT detalle_rol_pk PRIMARY KEY ( id_usuario,
+                                                                    id_rol );
+
+CREATE TABLE detalle_venta (
+    id_venta         INTEGER NOT NULL,
+    id_producto   INTEGER NOT NULL
+);
+
+ALTER TABLE detalle_venta ADD CONSTRAINT detalle_venta_pk PRIMARY KEY ( id_venta,
+                                                                        id_producto );
+
+CREATE TABLE producto (
+    id_producto   INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre        VARCHAR(200) NOT NULL,
+    descripcion   VARCHAR(500) NOT NULL,
+    precio        FLOAT NOT NULL
+);
+
+
+
+CREATE TABLE sede (
+    id_sede                  INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    alias                    VARCHAR(100) NOT NULL,
+    direccion                VARCHAR(500) NOT NULL,
+    id_usuario       INTEGER NOT NULL,
+    id_municipio   INTEGER NOT NULL
+);
+
+
+CREATE TABLE usuario (
+    id_usuario         INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    dpi                INTEGER NOT NULL,
+    nombre             VARCHAR(200) NOT NULL,
+    fecha_nacimiento   DATE NOT NULL,
+    correo             VARCHAR(200) NOT NULL,
+    password           VARCHAR(200) NOT NULL
+);
+
+
+CREATE TABLE venta (
+    id_venta             INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fecha_facturacion    DATE NOT NULL,
+    fecha_entrega        DATE,
+    id_cliente   INTEGER NOT NULL,
+    id_usuario   INTEGER NOT NULL
+);
+
+ALTER TABLE bodega
+    ADD CONSTRAINT bodega_sede_fk FOREIGN KEY ( id_sede )
+        REFERENCES sede ( id_sede );
+
+ALTER TABLE bodega
+    ADD CONSTRAINT bodega_usuario_fk FOREIGN KEY ( id_usuario )
+        REFERENCES usuario ( id_usuario );
+
+ALTER TABLE cliente
+    ADD CONSTRAINT cliente_sede_fk FOREIGN KEY ( id_sede )
+        REFERENCES sede ( id_sede );
+        
+ALTER TABLE detalle_productocategoria
+    ADD CONSTRAINT detalle_pcc_fk FOREIGN KEY ( id_categoria )
+        REFERENCES categoria ( id_categoria );
+
+ALTER TABLE detalle_productocategoria
+    ADD CONSTRAINT detalle_pcp_fk FOREIGN KEY ( id_producto )
+        REFERENCES producto ( id_producto );
+
+ALTER TABLE detalle_rol
+    ADD CONSTRAINT detalle_rol_rol_fk FOREIGN KEY ( id_rol )
+        REFERENCES rol ( id_rol );
+
+ALTER TABLE detalle_rol
+    ADD CONSTRAINT detalle_rol_usuario_fk FOREIGN KEY ( id_usuario )
+        REFERENCES usuario ( id_usuario );
+
+ALTER TABLE detalle_venta
+    ADD CONSTRAINT detalle_venta_producto_fk FOREIGN KEY ( id_producto )
+        REFERENCES producto ( id_producto );
+
+ALTER TABLE detalle_venta
+    ADD CONSTRAINT detalle_venta_venta_fk FOREIGN KEY ( id_venta )
+        REFERENCES venta ( id_venta );
+
+ALTER TABLE municipio
+    ADD CONSTRAINT municipio_departamento_fk FOREIGN KEY ( id_departamento )
+        REFERENCES departamento ( id_departamento );
+
+ALTER TABLE sede
+    ADD CONSTRAINT sede_municipio_fk FOREIGN KEY ( id_municipio )
+        REFERENCES municipio ( id_municipio );
+
+ALTER TABLE sede
+    ADD CONSTRAINT sede_usuario_fk FOREIGN KEY ( id_usuario )
+        REFERENCES usuario ( id_usuario );
+
+ALTER TABLE venta
+    ADD CONSTRAINT venta_cliente_fk FOREIGN KEY ( id_cliente )
+        REFERENCES cliente ( id_cliente );
+
+ALTER TABLE venta
+    ADD CONSTRAINT venta_usuario_fk FOREIGN KEY ( id_usuario )
+        REFERENCES usuario ( id_usuario );
